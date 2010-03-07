9f0905a9dba0e8c4a78599a7aa19e0471e8f2d4c
Thomas Kadauke <tkadauke@imedo.de>
2010-03-07 23:58:17 +0100

    less db queries

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -1,6 +1,6 @@
 class StartController < ApplicationController
   def index
-    @check_runs = CheckRun.recent
+    @check_runs = CheckRun.recent.all(:include => { :health_check => :site })
     render :partial => '/check_runs/activity' if request.xhr?
   end
 end

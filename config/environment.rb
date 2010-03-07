c256fee729463e48a6f7b508300503cecc10239c
Thomas Kadauke <tkadauke@imedo.de>
2010-03-07 16:36:56 +0100

    register gems in environment

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,8 @@ Rails::Initializer.run do |config|
   
   config.gem "acts_as_list"
   config.gem "gruff"
+  config.gem "webrat"
+  config.gem "mechanize"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

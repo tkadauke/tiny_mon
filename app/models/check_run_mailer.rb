dc42aa623b19aa096f78bc760a30b793b070f35d
Thomas Kadauke <tkadauke@imedo.de>
2010-03-08 14:29:35 +0100

    fix email subject

--- app/models/check_run_mailer.rb
+++ app/models/check_run_mailer.rb
@@ -1,6 +1,6 @@
 class CheckRunMailer < ActionMailer::Base
   def failure(check_run)
-    subject I18n.t("check_run_mailer.failure.subject", :health_check => check_run.health_check, :site => check_run.health_check.site)
+    subject I18n.t("check_run_mailer.failure.subject", :health_check => check_run.health_check.name, :site => check_run.health_check.site.name)
     recipients TinyMon::Config.recipient_email
     from TinyMon::Config.email_sender_address
     body :check_run => check_run

fe6ce4e1bb6ae9cf5a390707f52e294fec6f5bd2
Thomas Kadauke <tkadauke@imedo.de>
2010-03-10 00:34:30 +0100

    fix test

--- test/unit/submit_form_step_test.rb
+++ test/unit/submit_form_step_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class SubmitFormStepTest < ActiveSupport::TestCase
   test "should submit form" do
     step = SubmitFormStep.new
-    session = mock
+    session = mock(:log => nil, :submit_form => nil)
     step.run!(session)
   end
 end

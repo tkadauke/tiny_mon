ecb72d730bb89ae71dadb3ff89227d1ac3d68cb8
Thomas Kadauke <tkadauke@imedo.de>
2010-03-09 22:08:46 +0100

    show nice website output in case of an unmet expectation

--- app/helpers/check_runs_helper.rb
+++ app/helpers/check_runs_helper.rb
@@ -1,2 +1,17 @@
 module CheckRunsHelper
+  def format_log_message(message)
+    case message
+    when /<\/html>/
+      format_embedded_html_page(message)
+    else
+      content_tag(:code) { message }
+    end
+  end
+  
+  def format_embedded_html_page(message)
+    id = "msg#{message.object_id}"
+    html = "<iframe id=#{id}></iframe>"
+    html << javascript_tag("var doc=$('#{id}').contentDocument;doc.open(); doc.write('#{escape_javascript(message)}');doc.close()")
+    html
+  end
 end

63d5551784cb1adc867352893daa982737fe726e
Thomas Kadauke <tkadauke@imedo.de>
2010-06-30 03:01:09 +0200

    nicer sender address in check run mailer

--- app/models/check_run_mailer.rb
+++ app/models/check_run_mailer.rb
@@ -2,7 +2,7 @@ class CheckRunMailer < ActionMailer::Base
   def failure(check_run)
     subject I18n.t("check_run_mailer.failure.subject", :health_check => check_run.health_check.name, :site => check_run.health_check.site.name)
     recipients TinyMon::Config.recipient_email
-    from TinyMon::Config.email_sender_address
+    from "TinyMon <#{TinyMon::Config.email_sender_address}>"
     body :check_run => check_run
   end
 end

fb364df33982da13fad56a1986f49dc43cf3c2d4
Thomas Kadauke <tkadauke@imedo.de>
2010-06-30 02:40:38 +0200

    added some tests

--- test/functional/user_sessions_controller_test.rb
+++ test/functional/user_sessions_controller_test.rb
@@ -1,8 +1,27 @@
 require 'test_helper'
 
 class UserSessionsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+  
+  test "should create user session" do
+    john = User.create(:full_name => 'John Doe', :email => 'john.doe@example.com', :password => '12345', :password_confirmation => '12345')
+    
+    post :create, :user_session => { :email => 'john.doe@example.com', :password => '12345' }
+    assert user_session = UserSession.find
+    assert_equal john, user_session.user
+    assert_redirected_to root_path
+  end
+  
+  test "should destroy user session" do
+    john = User.create(:full_name => 'John Doe', :email => 'john.doe@example.com', :password => '12345', :password_confirmation => '12345')
+    
+    login_with john
+    
+    delete :destroy
+    assert_nil UserSession.find
+    assert_redirected_to root_path
   end
 end
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,8 +1,40 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+  
+  test "should create user" do
+    assert_difference 'User.count' do
+      post :create, :user => { :full_name => "John Doe", :password => "12345", :password_confirmation => "12345", :email => "john@doe.com" }
+    end
+    
+    assert_redirected_to root_path
+  end
+  
+  test "should show user" do
+    john = User.create(:full_name => 'John Doe', :email => 'john.doe@example.com', :password => '12345', :password_confirmation => '12345')
+    login_with john
+    
+    get :show, :id => john.id
+    assert_response :success
+  end
+
+  test "should get edit" do
+    john = User.create(:full_name => 'John Doe', :email => 'john.doe@example.com', :password => '12345', :password_confirmation => '12345')
+    login_with john
+
+    get :edit, :id => john.id
+    assert_response :success
+  end
+
+  test "should update user" do
+    john = User.create(:full_name => 'John Doe', :email => 'john.doe@example.com', :password => '12345', :password_confirmation => '12345')
+    login_with john
+
+    put :update, :id => john.id, :user => { }
+    assert_redirected_to root_path
   end
 end

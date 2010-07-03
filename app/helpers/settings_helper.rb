66d29c32b711ee4ee7badb2c9c8256fddff7cafa
Thomas Kadauke <tkadauke@imedo.de>
2010-07-03 02:21:47 +0200

    dryed up settings form

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -1,2 +1,9 @@
 module SettingsHelper
+  def translate_if_possible(text_or_hash)
+    if text_or_hash.is_a?(Hash)
+      text_or_hash[I18n.locale.to_s]
+    else
+      text_or_hash
+    end
+  end
 end

855a6ef69b837288d77c4ef165104163c6dd4823
Thomas Kadauke <tkadauke@imedo.de>
2010-07-16 15:38:42 +0200

    speed up migration

--- db/migrate/20100715212526_add_account_id_to_check_runs.rb
+++ db/migrate/20100715212526_add_account_id_to_check_runs.rb
@@ -6,8 +6,11 @@ class AddAccountIdToCheckRuns < ActiveRecord::Migration
     add_index :check_runs, [:account_id, :created_at]
     
     CheckRun.reset_column_information
-    CheckRun.find(:all, :include => { :health_check => :site }).each do |check_run|
-      check_run.update_attribute(:account_id, check_run.health_check.site.account_id) rescue nil
+    
+    Account.find(:all).each do |account|
+      result = ActiveRecord::Base.connection.select_all("SELECT `check_runs`.`id` FROM `check_runs` LEFT OUTER JOIN `health_checks` ON `health_checks`.id = `check_runs`.health_check_id LEFT OUTER JOIN `sites` ON `sites`.id = `health_checks`.site_id WHERE (sites.account_id = #{account.id})")
+      check_run_ids = result.collect {|p|p.values}.flatten
+      CheckRun.update_all "account_id = #{account.id}", ["id in (?)", check_run_ids]
     end
   end
 

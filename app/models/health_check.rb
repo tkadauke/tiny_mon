a263c5f0ed9e41906a84d4560364318976a25c0c
Thomas Kadauke <tkadauke@imedo.de>
2010-07-22 23:31:34 +0200

    fix nasty bug in health check filtering

--- app/models/health_check.rb
+++ app/models/health_check.rb
@@ -19,7 +19,7 @@ class HealthCheck < ActiveRecord::Base
   
   def self.find_for_list(filter, find_options)
     with_search_scope(filter) do
-      find(:all, find_options)
+      find(:all, find_options.merge(:include => {:site => :account}))
     end
   end
 
@@ -86,8 +86,21 @@ protected
   end
 
   def self.with_search_scope(filter, &block)
-    conditions = filter.empty? ? nil : ['health_checks.name LIKE ? OR sites.name LIKE ?', "%#{filter.query}%", "%#{filter.query}%"]
-    with_scope :find => { :conditions => conditions, :include => :site } do
+    conditions = if filter.empty?
+      nil
+    else
+      # See activerecord/lib/active_record/associations.rb, line 1666
+      #
+      # For the SQL LIKE condition, we need to get rid of dots in the filter query, because ActiveRecord thinks
+      # everything left of a dot is a referenced table, which results in SQL errors (few filter queries are actual
+      # table names). This has the subtle consequence that a dot in the search field matches any character in the
+      # SQL result set, whereas in the frontend only actual dots get highlighted. We replace dots with underscores,
+      # which match a single character in SQL LIKE queries.
+      sql_like = filter.query.gsub('.', '_')
+      ['health_checks.name LIKE ? OR sites.name LIKE ?', "%#{sql_like}%", "%#{sql_like}%"]
+    end
+    
+    with_scope :find => { :conditions => conditions } do
       yield
     end
   end
--- config/environment.rb
+++ config/environment.rb
@@ -48,3 +48,5 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+Rails.backtrace_cleaner.remove_silencers!

ba68e25faa8b02c44347ff825ce8f52991871315
Thomas Kadauke <tkadauke@imedo.de>
2010-08-04 00:36:59 +0200

    reenable emails

--- app/models/check_run_mailer.rb
+++ app/models/check_run_mailer.rb
@@ -1,8 +1,5 @@
 class CheckRunMailer < ActionMailer::Base
   def failure(check_run, user)
-    # email for other users disabled temporarily
-    return if user.id != 1
-    
     subject I18n.t("check_run_mailer.failure.subject", :health_check => check_run.health_check.name, :site => check_run.health_check.site.name)
     recipients user.email
     from "TinyMon <#{TinyMon::Config.email_sender_address}>"

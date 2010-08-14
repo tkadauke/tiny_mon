244258fec21bcf749df53d4cf186decfa5e2f2cc
Thomas Kadauke <tkadauke@imedo.de>
2010-08-14 05:13:12 +0200

    use less gem

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
   config.gem "background_lite", :lib => false
   config.gem "will_paginate"
   config.gem "fastercsv"
+  config.gem "less"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

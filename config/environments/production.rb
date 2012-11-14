bd5f414de064628c3edd00b3198f633957c01934
Thomas Kadauke <tkadauke@imedo.de>
2012-11-14 01:06:36 +0100

    Try to make asset pipeline work. CSS precompiling crashes the precompile task and no hint on stackoverflow is helping :(

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,17 +34,13 @@ TinyMon::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
 
   config.assets.css_compressor = :yui
   config.assets.js_compressor = :uglifier
-
-  config.assets.precompile += %w(
-    style.css
-  )
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"

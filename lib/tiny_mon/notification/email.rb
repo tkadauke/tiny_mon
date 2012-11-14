f1bf77c4b1ae804dfe08a5e2012459015d544e69
Thomas Kadauke <tkadauke@imedo.de>
2012-11-14 13:24:02 +0100

    Port email notifications to Rails 3

--- lib/tiny_mon/notification/email.rb
+++ lib/tiny_mon/notification/email.rb
@@ -2,11 +2,11 @@ module TinyMon
   module Notification
     class Email < Base
       def deliver_success(check_run, user)
-        CheckRunMailer.deliver_success(check_run, user)
+        CheckRunMailer.success(check_run, user).deliver
       end
       
       def deliver_failure(check_run, user)
-        CheckRunMailer.deliver_failure(check_run, user)
+        CheckRunMailer.failure(check_run, user).deliver
       end
     end
   end

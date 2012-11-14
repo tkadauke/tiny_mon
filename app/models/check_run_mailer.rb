38cdf7d6faeefb6c2e356d83cd17023b441dd72b
Thomas Kadauke <tkadauke@imedo.de>
2012-11-14 21:51:14 +0100

    Fix success mails

--- app/models/check_run_mailer.rb
+++ app/models/check_run_mailer.rb
@@ -8,9 +8,8 @@ class CheckRunMailer < ActionMailer::Base
   end
 
   def success(check_run, user)
-    subject I18n.t("check_run_mailer.success.subject", :health_check => check_run.health_check.name, :site => check_run.health_check.site.name)
-    recipients user.email
-    from "TinyMon <#{TinyMon::Config.email_sender_address}>"
-    body :check_run => check_run
+    @check_run = check_run
+    
+    mail :to => user.email, :subject => I18n.t("check_run_mailer.success.subject", :health_check => check_run.health_check.name, :site => check_run.health_check.site.name)
   end
 end

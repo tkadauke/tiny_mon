44dec6b45700df2b7814e70fd31aa998ee3de7ef
Thomas Kadauke <tkadauke@imedo.de>
2013-05-09 13:22:56 +0200

    Only show first 8 characters for deployment revision to avoid breaking the layout on phones

--- app/helpers/deployments_helper.rb
+++ app/helpers/deployments_helper.rb
@@ -1,5 +1,5 @@
 module DeploymentsHelper
   def deployment_title(deployment)
-    I18n.t("deployment.revision_and_time", :revision => deployment.revision, :time => time_ago_in_words(deployment.created_at))
+    I18n.t("deployment.revision_and_time", :revision => deployment.revision[0..7], :time => time_ago_in_words(deployment.created_at))
   end
 end

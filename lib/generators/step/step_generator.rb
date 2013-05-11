e013644d775c1fb228e0a4fd45f229eeb2d6ca72
Thomas Kadauke <tkadauke@imedo.de>
2013-05-11 12:58:58 +0200

    Port generator to Rails 3

--- lib/generators/step/step_generator.rb
+++ lib/generators/step/step_generator.rb
@@ -1,20 +1,13 @@
-class StepGenerator < Rails::Generator::NamedBase
-  def manifest
-    record do |m|
-      # Check for class naming collisions.
-      m.class_collisions "#{class_name}Step", "#{class_name}StepTest"
-
-      # Model, test, and view directories.
-      m.directory 'app/steps'
-      m.directory 'app/views/steps'
-      m.directory 'test/unit'
-
-      # Model class, unit test and views
-      m.template 'step.rb',       File.join('app/steps', "#{file_name}_step.rb")
-      m.template 'step.html.erb', File.join('app/views/steps', "_#{file_name}_step.html.erb")
-      m.template 'form.html.erb', File.join('app/views/steps', "_#{file_name}_step_form.html.erb")
-      m.template 'unit_test.rb',  File.join('test/unit', "#{file_name}_step_test.rb")
-    end
+class StepGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path("../templates", __FILE__)
+  argument :attributes, type: :array, default: [], banner: "field[:type] field[:type]"
+  check_class_collision suffix: "Controller"
+  
+  def create_step_files
+    template 'step.rb',       File.join('app/steps', "#{file_name}_step.rb")
+    template 'step.html.erb', File.join('app/views/steps', "_#{file_name}_step.html.erb")
+    template 'form.html.erb', File.join('app/views/steps', "_#{file_name}_step_form.html.erb")
+    template 'unit_test.rb',  File.join('test/unit', "#{file_name}_step_test.rb")
   end
 
 protected
--- lib/generators/step/templates/step.rb
+++ lib/generators/step/templates/step.rb
@@ -1,8 +1,10 @@
 class <%= class_name %>Step < Step
+<% unless attributes.empty? -%>
 <% for attribute in attributes -%>
   property :<%= attribute.name %>, :<%= attribute.type %>
 <% end -%>
   
+<% end -%>
   def run!(session, check_run)
   end
 end

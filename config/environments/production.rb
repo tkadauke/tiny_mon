9f54fe1cb43074acdd2bc68c40c993904e98c0d4
Jan Renz <jan.renz@gmail.com>
2014-05-27 10:05:30 +0200

    Asset debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,8 @@ TinyMon::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
+  config.action_dispatch.x_sendfile_header = "X-Accel-Redirect"
 
   # Generate digests for assets URLs
   config.assets.digest = true

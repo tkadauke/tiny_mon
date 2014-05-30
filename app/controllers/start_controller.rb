f4ba9d4a93b3d040f39e56f77f03481b5a4badd8
Thomas Kadauke <thomas.kadauke@googlemail.com>
2014-05-31 00:05:56 +0200

    Fix horrible dashboard performance

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -7,7 +7,7 @@ class StartController < ApplicationController
     if @account
       # disable account_id check because it's to slow
       # :conditions => ["sites.account_id = ?", @account.id]
-      @check_runs = @account.check_runs.recent.where(:account_id => @account.id).order('started_at DESC').includes(:health_check )
+      @check_runs = @account.check_runs.recent.includes(:health_check)
       @upcoming_health_checks = @account.health_checks.upcoming.limit(10)
       render :partial => 'dashboard' if request.xhr?
     end

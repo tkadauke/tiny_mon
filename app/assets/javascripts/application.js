5eaa4a8cf198756e6ace9f0664e4fce84fc6100a
Jan Renz <jan.renz@gmail.com>
2014-06-24 18:55:12 +0200

    Same height for every heath check in overview page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,4 +11,5 @@
 //= require jqplot/plugins/dateAxisRenderer
 //= require jqplot/plugins/highlighter
 //= require bootstrap
+//= require equal_heights
 //= require_tree .
---
+++ app/assets/javascripts/equal_heights.js
@@ -0,0 +1,50 @@
+/*!
+ * Simple jQuery Equal Heights
+ *
+ * Copyright (c) 2013 Matt Banks
+ * Dual licensed under the MIT and GPL licenses.
+ * Uses the same license as jQuery, see:
+ * http://docs.jquery.com/License
+ *
+ * @edited by Jan Renz
+ */
+(function($) {
+
+    $.fn.equalHeights = function(minWidth) {
+        var maxHeight = 0,
+            $this = $(this);
+        $this.each( function() {
+            if (!minWidth || minWidth == 'undefined'){
+                minWidth=0;
+            }
+            $this.css('min-height', '0px');
+            if ($(window).innerWidth() <   minWidth){
+                maxHeight = '0';
+            }else{
+                var height = $(this).innerHeight();
+                if ( height > maxHeight ) { maxHeight = height; }
+            }
+        });
+
+        return $this.css('min-height', maxHeight);
+    };
+    $(document).ready(function(){
+        // auto-initialize plugin
+        $('[data-equal]').each(function(){
+            var $this = $(this),
+                target = $this.data('equal');
+                minwidth = $this.data('equal-minwidth');
+            $this.find(target).equalHeights(minwidth);
+        });
+
+    });
+    $(window).on("resize", function(){
+        $('[data-equal]').each(function(){
+            var $this = $(this),
+                target = $this.data('equal');
+                minwidth = $this.data('equal-minwidth');
+            $this.find(target).equalHeights(minwidth);
+        });
+    });
+
+})(jQuery);

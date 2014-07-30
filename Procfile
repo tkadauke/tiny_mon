affcabead333c6f35c15fae4cbecb8e541e5485d
Jan Renz <jan.renz@gmail.com>
2014-07-30 20:46:25 +0200

    Start scheduler from application init

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 #!/bin/bash
 web: bundle exec rails server -p $PORT
-worker: cd /app && bundle exec rake scheduler:start
---
+++ config/initializers/scheduler.rb
@@ -0,0 +1,6 @@
+def call_rake (task, options = {})
+  options[:rails_env] ||= Rails.env
+  args = options.map { |n, v| "#{n.to_s.upcase}='#{v}'" }
+  system "rake #{task} #{args.join(' ')} --trace 2>&1 >> #{Rails.root}/log/rake.log &"
+end
+call_rake "scheduler:start"

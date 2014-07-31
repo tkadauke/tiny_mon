f902685ca6aa8d4477791f6cfbae32dcf88d9341
Jan Renz <jan.renz@gmail.com>
2014-07-31 21:31:59 +0200

    memory debugging

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 #!/bin/bash
 web: bundle exec rails server -p $PORT
+worker: bundle exec rake scheduler:start
--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,4 +3,4 @@ def call_rake (task, options = {})
   args = options.map { |n, v| "#{n.to_s.upcase}='#{v}'" }
   system "rake #{task} #{args.join(' ')} --trace 2>&1 >> #{Rails.root}/log/rake.log &"
 end
-call_rake "scheduler:start" unless $run_from_rake
+#call_rake "scheduler:start" unless $run_from_rake

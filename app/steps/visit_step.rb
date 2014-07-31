923a274058af1f309ac927fbd5ee6b202f46c3ce
Jan Renz <jan.renz@gmail.com>
2014-07-31 10:00:35 +0200

    Debugging

--- app/steps/visit_step.rb
+++ app/steps/visit_step.rb
@@ -1,10 +1,16 @@
 class VisitStep < Step
   property :url, :string
-  
+  property :check_http_code, :boolean
   validates_presence_of :url
   
   def run!(session, check_run)
+    # Add DoNotTrack header
     session.driver.add_headers("DNT" => "1")
     session.visit(self.url)
+    #TODO Check if http response state != 200
+    status_code = session.status_code
+    if status_code != 200   property :negate, :boolean
+      session.fail ContentCheckFailed, "Expected http status to be 200, but received #{status_code}"
+    end
   end
 end

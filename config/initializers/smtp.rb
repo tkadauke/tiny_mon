d85afc8275eabde09e67e37fbe38cd1dec523af9
Jan Renz <jan.renz@gmail.com>
2014-08-01 08:32:01 +0200

    Try to fix SMTP Issue

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :domain => TinyMon::Config.email_domain,
-  :address => TinyMon::Config.email_address,
-  :port => TinyMon::Config.email_port,
-  :user_name => TinyMon::Config.email_user_name,
-  :password => TinyMon::Config.email_password,
+  :domain => TinyMon::Config.email_domain.to_s,
+  :address => TinyMon::Config.email_address.to_s,
+  :port => TinyMon::Config.email_port.to_i,
+  :user_name => TinyMon::Config.email_user_name.to_s,
+  :password => TinyMon::Config.email_password.to_s,
   :authentication => TinyMon::Config.email_authentication.to_sym,
   :openssl_verify_mode => TinyMon::Config.email_openssl_verify_mode.to_sym,
   :enable_starttls_auto => TinyMon::Config.email_enable_starttls_auto

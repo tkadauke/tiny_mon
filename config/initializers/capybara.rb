a9790b31349222e2d91147a08f9b79bbe4339a9d
Jan Renz <jan.renz@hpi.de>
2014-09-06 12:51:52 +0200

    Set our host and increase timeout

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -1,3 +1,4 @@
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, :js_errors => false)
+  Capybara::Poltergeist::Driver.new(app, :js_errors => false, :timeout => 60)
+  #this is quite a large timeout, mazbe we should increas this only on a per site base, for sites far away
 end

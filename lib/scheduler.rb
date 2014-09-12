d294b0e67e4f7ecaa9dd6877f8639767eecbb71a
Jan Renz <jan.renz@hpi.de>
2014-09-12 21:45:54 +0200

    revert  threading

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -9,10 +9,8 @@ class Scheduler
   def self.check_all
     HealthCheck.recover_zombies
     HealthCheck.due.each do |check|
-      Thread.new {
-        check.prepare_check!
-        check.check!
-      }
+      check.prepare_check!
+      check.check!
     end
   end
 end

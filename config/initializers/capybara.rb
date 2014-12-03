3d32c554cc882cbb6e706ca8ce716252efbc4b58
Jan Renz <jan.renz@hpi.de>
2014-12-03 22:05:27 +0100

    debugging

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -1,5 +1,6 @@
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, :js_errors => false, :timeout => 600, :phantomjs_options => ['--ignore-ssl-errors=yes'])
+
   #this is quite a large timeout, maybe we should increase this only on a per site base, for sites far away
 end
 Capybara.default_wait_time = 30
